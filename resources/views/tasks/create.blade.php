c680303a73d788a147ddbc082c3d003c4a2a9782
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-04-16 00:09:26 +0600

    Add form structure for creating new tasks with CSRF protection

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,14 @@
 
 <h2>Add New Tasks</h2>
 
-<form>
+<form method="POST" action="" enctype="multipart/form-data">
+    @csrf
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" class="form-control" placeholder="Enter Task Title">
+    </div>
+
+
 
 
 </form>
