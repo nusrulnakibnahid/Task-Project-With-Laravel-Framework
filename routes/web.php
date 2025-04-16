ca880d1977554849943ea4550d661dd3815e48b9
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-04-16 20:11:13 +0600

    Add delete route for task deletion in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\TaskController;
 Route::get('/',[TaskController::class,'index'])->name('tasks.index');
 Route::get('/tasks/create',[TaskController::class,'create'])->name('tasks.create');
 Route::post('/tasks',[TaskController::class,'store'])->name('tasks.store');
+Route::delete('/tasks/{id}/delete',[TaskController::class,'destroy'])->name('tasks.destroy');
 
 
 
